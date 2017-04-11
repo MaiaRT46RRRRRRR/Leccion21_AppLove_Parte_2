8e5cbeb4206d1a3f9f5fc520dd534d430ed22ee0
Maia <maia.rt.46@gmail.com>
2017-04-11 15:57:41 -0500

    Añadiendo funcionalidad

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,13 @@
 var img =document.getElementsByClassName('img');
-
- 	for(var i=1;i<img.length;i++){
- 		img[i].addEventListener("click",function(e){
- 			console.log(this);
- 			document.getElementById("muestra").setAttribute("src","assets/img/"+ "img-"+[i]+".jpg");
+var showModal=function(e){
+ 			document.getElementById("muestra").src=this.firstElementChild.src
+ 			// this.firstElementChild.setAttribute("src","assets/img/"+ "img-"+(i)+".jpg");
 			document.getElementById("normal").style.display="block";
-	    });
+	    };
+ 	for(var i=0;i<img.length;i++){
+ 		img[i].addEventListener("click",showModal);
     }
-
 	
-document.getElementById("normal").addEventListener("click",function (e){
-		document.getElementById("cerrar").style.display="none";
+document.getElementById("cerrar").addEventListener("click",function (e){
+		document.getElementById("normal").style.display="none";
 });	
